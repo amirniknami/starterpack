470b682e4941f92ee535f17965357b23770aee1f
amir <amirniknami@gmail.com>
2017-03-24 01:53:38 +0430

    everything works fine

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+module.exports ={
+    entry:{
+        app:[
+          './src/js/main.js',
+          './src/scss/main.scss'
+        ],
+    },
+    output:{
+        path:'./app',
+        filename:'./js/[name].js'
+    },
+    module:{
+        rules:[
+         {
+                 test: /\.s[ac]ss$/,
+                use: ExtractTextPlugin.extract({
+                  use: [
+                  {
+                    loader:'css-loader',
+                    options:{url:false,minimize:true}
+                  },
+                  'sass-loader'
+                  ],
+                  fallback: 'style-loader'
+                })
+         }
+        ]
+    },
+     plugins:[
+    new ExtractTextPlugin("./css/[name].css"),
+    new webpack.optimize.UglifyJsPlugin()
+    ]
+};
