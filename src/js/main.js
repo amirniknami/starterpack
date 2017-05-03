a76a614f0bedb7ff70adda5e5698a73ab3216953
amir <amirniknami@gmail.com>
2017-05-03 19:08:44 +0430

    v1.1 jquery loaded and some bug has been fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-alert("ok");
+//loading jquery for your project.have fun! 
+//remove it if you are not using jquery
+var $ = require('jquery');
+$('document').ready(function(){
+    alert('if you see this message it means the jquery has been loaded correctly and you are ready to build something amazing!');
+});
