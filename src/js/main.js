27e2df80ce0929981fdd228cbaadca9b8e425572
amir <amirniknami@gmail.com>
2017-05-03 19:10:28 +0430

    v1.11 a bug has been fixed

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,5 @@
 //remove it if you are not using jquery
 var $ = require('jquery');
 $('document').ready(function(){
-    alert('if you see this message it means the jquery has been loaded correctly and you are ready to build something amazing!');
+    console.log('jquery loaded succesfuly');
 });
